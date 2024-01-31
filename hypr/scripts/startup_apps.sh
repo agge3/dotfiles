6421ed6de28d9a053ca15aef97f9d7acc7e89771
agge3 <sterspark@gmail.com>
2024-01-31 10:24:09 -0800

    added zshrc and p10k

--- hypr/scripts/startup_apps.sh
+++ hypr/scripts/startup_apps.sh
@@ -4,11 +4,11 @@
 exec gentoo-pipewire-launcher &
 
 # sway lock screen and idle
-exec swayidle -w \
-    timeout 300 'swaylock -f' \
-    timeout 600 'hyprctl dispatch dpms off' \
-    resume 'hyprctl dispatch dpms on' \
-    before-sleep 'swaylock -f' &
+#exec swayidle -w \
+#    timeout 300 'swaylock -f' \
+#    timeout 600 'hyprctl dispatch dpms off' \
+#    resume 'hyprctl dispatch dpms on' \
+#    before-sleep 'swaylock -f' &
 
 # polkit - kde is best (gentoowiki)
 exec /usr/lib64/libexec/polkit-kde-authentication-agent-1 &
