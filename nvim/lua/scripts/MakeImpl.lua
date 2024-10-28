e27c89be7c7851376cb53a5e7adfa2622bb6c26f
agge3 <sterspark@gmail.com>
2024-10-28 05:06:34 -0700

    Added more to NeoVim MakeImpl.lua

--- nvim/lua/scripts/MakeImpl.lua
+++ nvim/lua/scripts/MakeImpl.lua
@@ -1,4 +1,17 @@
 --
+-- Public interface to call script and make CXX implementation file stubs from
+-- a currently opened CXX header file NeoVim buffer.
+--
+function MakeImpl()
+	local decl = parseHeader()
+	generateImpl(decl)
+end
+
+--
+-- Private utility functions.
+--
+
+--
 -- Returns a trimmed string. Default is to trim whitespace.
 --
 -- PARAM: String s, the string to trim.
@@ -77,7 +90,60 @@ local function captureParen(words)
 	return new_words
 end
 
-local function processLine(line, decl)
+--
+-- Private driver classes.
+--
+
+local generator = {}
+generator.__index = generator
+
+-- Wrapper for Lua write() best practice. For a simpler write call.
+-- CREDIT: FreeBSD: Refactor makesyscalls.lua #1362
+function generator:write(line)
+	assert(self.gen:write(line))
+end
+
+function generator:doxygenHeader(desc)
+    local date = os.date("%Y-%m-%d")
+	local user = os.getenv("USER") or os.getenv("USERNAME") or "Your Name" 
+
+    gen:write(string.format([[
+/**
+ * @file %s
+ *
+ * This file contains the implementation of the class methods
+ * defined in %s.
+ *
+ * @date %s
+ * @author %s
+ */
+
+]], filename, desc or "Class implementation", filename, date, user))
+end
+
+-- generator binds to the parameter file.
+-- CREDIT: FreeBSD: Refactor makesyscalls.lua #1362
+function generator:new(obj, fh)
+	obj = obj or {}
+	setmetatable(obj, self)
+	self.__index = self
+
+	self.gen = assert(io.open(fh, "w+"))
+
+	return obj
+end
+
+local make_impl = {}
+make_impl.__index = make_impl
+
+function make_impl:init()
+	self.buf = vim.api.nvim_get_current_buf
+	self.fh = vim.api.nvim_buf_get_name(buf)
+	self.fh = fh:match("(.+)%..+$") .. ".h"
+	self.impl_fh = fh:gsub("%.h$", ".cpp")
+end
+
+function make_impl:processLine(line, key)
 	words = split(line)
 	-- XXX This is where the many ways something can be declared in CXX
 	-- headers can cause a lot of variations. This handling is the most
@@ -95,34 +161,50 @@ local function processLine(line, decl)
 	if words[#words] == "const" then
 		local s = words[#words - 1] .. " " .. words[#words]
 	end
-	table.insert(decl, {
+	table.insert(self.decl[key], {
 		ret = words[1 + ret_offset],
 		func = words[2 + func_offset],
 	})
 end
 
-local function MakeImpl()
+function make_impl:parseHeader()
 	-- Parse file until we reach the `class` keyword.
-	local buf = vim.api.nvim_get_current_buf()
-	local lines = vim.api.nvim_buf_get_lines(buf, 0, -1 false)
+	local lines = vim.api.nvim_buf_get_lines(self.buf, 0, -1 false)
 
 	local copying = false
-	local decl = {}
+	local key = ""
+	local template_decl = ""
 	-- XXX Handle multi-line declarations.
 	local found_scolon = false
 	local multi_line = ""
 	for num, line in ipairs(lines) do
 		local trimmed = trim(line)
-		if trimmed:find("class") then
+
+        -- Detect template declarations.
+        if trimmed:find("^template%s*<") then
+            template_decl = trimmed 	-- Store template declaration.
+            goto next
+        end
+
+		if trimmed:find("^class") or trimmed:find("^struct") then
 			copying = true
 			local l = split(line)
-			local key = l[2]
+			key = l[2]
 			-- Add this class name as a key in our declarations table.
-			decl[key] = nil
+			self.decl[key] = {}
+			if template_decl ~= "" then
+				table.insert(self.decl[key], {
+					template = template_decl
+				})
+			end
+			-- Reset template declaration (if there was one), because each
+			-- class/struct has its own template declaration (or lack thereof).
+			template_decl = ""
 			goto next	-- Go to next line.
 		end
 		if trimmed:find("};") then
 			copying = false
+			key = ""
 			goto next	-- Go to next line.
 		end
 		if copying and line ~= "" then
@@ -130,15 +212,83 @@ local function MakeImpl()
 				found_scolon = true
 			end
 			if found_scolon and multi_line == "" then
-				processLine(line, decl)
+				processLine(line, decl, key)
+				multi_line = ""
+			elseif found_scolon and multi_line ~= "" then	
+				processLine(line, decl, key)
 				multi_line = ""
 			elseif not found_scolon then
 				multi_line = multi_line .. line
-			elseif found_scolon and multi_line ~= "" then	
-				processLine(line, decl)
-				multi_line = ""
 			end
 		end
 		::next::
 	end
+end
+
+function make_impl:processRetStatement(ret)
+	local ret_statement = ""
+	local found_ptr = ret:find("*")
+	local found_strarr = ret:find("**")
+
+	if ret:find("void") ~= nil and found_ptr ~= nil then
+		-- do nothing
+		ret_statement = ""
+	-- This `find()` also handles special int types, like uint32_t, etc.
+	elseif ret:find("int") ~= nil and found_ptr == nil then
+		ret_statement = "return 0;"
+	elseif ret:find("optional<") ~= nil and found_ptr == nil then
+		ret_statement = "return std::nullopt;"
+	elseif ret:find("float") ~= nil and found_ptr == nil then
+		ret_statement = "return 0.f;"
+	elseif ret:find("double") ~= nil and found_ptr == nil then
+		ret_statement = "return 0.0;"
+	elseif ret:find("bool") ~= nil and found_ptr == nil then
+		ret_statement = "return false;"
+	elseif ret:find("string") ~= nil and found_ptr == nil then
+		ret_statement = "return \"\";"
+	elseif found_ptr ~= nil and found_strarr == nil then
+		ret_statement = "return nullptr"
+	end
+
+	return ret_statement
+end
+
+function make_impl:generateImpl(decl)
+	-- XXX Find pwd -> `../` -> find src/source/etc. dir pattern -> create .cpp
+	-- in dir, or create .cpp in `.` if failed.
+	local gen = generator:new({}, self.impl_fh)
+
+	-- Write out Doxygen-style file header.
+	gen:doxygenHeader()
+
+	-- Write out include for the header we just processed.
+	gen:write(string.format("#include \"%s\"\n\n"))
+
+	for k, v in pairs(self.decl) do
+		for _, fn in pairs(v) do
+			local r = processRetStatement(fn.ret)
+			local f = strip(fn.func, ";")
+			if fn.template ~= nil then
+				gen:write(string.format("%s\n"), fn.template)
+			end
+			-- xxx handle class definition
+			gen:write(string.format([[
+%s %s::%s
+{
+	%s
+}
+
+]]), fn.ret, k, fn.func, r)
+		end
+	end
+end
+
+function make_impl:new(obj)
+	obj = obj or {}
+	setmetatable(obj, self)
+	self.__index = self
+
+	self.decl = {}
+
+	self:init()
 end
